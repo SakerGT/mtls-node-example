7a9ebf9510543b8db074bbace09d1b9df1db4124
Nigel Somerfield <nigel@somerfield.co.nz>
2020-05-08 15:11:02 +1200

    Created common name checks

---
+++ common-name/README.md
@@ -0,0 +1,50 @@
+# Subject common name
+
+This example builds on the [basic](../basic/README.md) scenario, and adds:
+
+* Client checks a static hostname (FQDN) string against server certificate Subject CN or Subject Alt Names.
+* Server checks a static hostname (FQDN) string against client certificate
+
+## Run the example to see failure
+
+First run the server with `node server.js`
+
+Secondly, run the client `node client.js`
+
+The output should be a TLS handshake failure, where the client is rejecting the server as not matching the hostname expected.
+
+## Update the client
+
+Change the following code in `client.js`
+
+```javascript
+//const err = tls.checkServerIdentity("www.demoservers.co.nz", cert);
+const err = tls.checkServerIdentity("incorrect.url.co.nz", cert);
+```
+
+to look like the following:
+
+```javascript
+const err = tls.checkServerIdentity("www.demoservers.co.nz", cert);
+//const err = tls.checkServerIdentity("incorrect.url.co.nz", cert);
+```
+
+Save your change and re-run the client.  You should now have a different error, a HTTP 401 from the server.
+
+## Update the server
+
+The last step is update the server to accept the client subject CN.  Change the following in `server.js`
+
+```javascript
+const clientCN = "incorrect.demo.co.nz"
+//const clientCN = 'client.demo.co.nz';
+```
+
+To look like:
+
+```javascript
+//const clientCN = "incorrect.demo.co.nz"
+const clientCN = 'client.demo.co.nz';
+```
+
+Save your change and re-start the server, then re-run the client.  Everything should now work as expected, and a HTTP 200 is returned to the client.
---
+++ common-name/client.js
@@ -0,0 +1,67 @@
+const tls = require('tls');
+const https = require('https');
+const crypto = require('crypto');
+const fs = require('fs');
+
+function sha256(s) {
+  return crypto.createHash('sha256').update(s).digest('base64');
+}
+const options = {
+  hostname: 'localhost',
+  port: 8000,
+  path: '/',
+  method: 'GET',
+  key: fs.readFileSync('../CA/client1-key.pem'),
+  cert: fs.readFileSync('../CA/client1-crt.pem'),
+  ca: fs.readFileSync('../CA/ca-crt.pem'),
+
+  checkServerIdentity: function(host, cert) {
+    // Make sure the certificate is issued to the host we are connected to
+    
+    
+    //const err = tls.checkServerIdentity("www.demoservers.co.nz", cert);
+    const err = tls.checkServerIdentity("incorrect.url.co.nz", cert);
+    if (err) {
+      return err;
+    }
+    
+
+    // This loop is informational only.
+    // Print the certificate and public key fingerprints of all certs in the
+    // chain. Its common to pin the public key of the issuer on the public
+    // internet, while pinning the public key of the service in sensitive
+    // environments.
+    do {
+      console.log('Subject Common Name:', cert.subject.CN);
+      console.log('  Certificate SHA256 fingerprint:', cert.fingerprint256);
+
+      hash = crypto.createHash('sha256');
+      console.log('  Public key ping-sha256:', sha256(cert.pubkey));
+      console.log('  Key bits:', cert.bits);
+      console.log('  Valid from:',cert.valid_from);
+      console.log('  Valid to:  ',cert.valid_to);
+
+      lastprint256 = cert.fingerprint256;
+      cert = cert.issuerCertificate;
+    } while (cert.fingerprint256 !== lastprint256);
+
+  },
+};
+
+options.agent = new https.Agent(options);
+const req = https.request(options, (res) => {
+  console.log('All OK. Server matched our pinned Subject CN');
+  console.log('statusCode:', res.statusCode);
+  // Print the HPKP values
+  console.log('headers:', res.headers['public-key-pins']);
+
+  res.on('data', (d) => {
+    console.log('Server data:', d.toString());
+  });
+
+});
+
+req.on('error', (e) => {
+  console.error(e.message);
+});
+req.end();
